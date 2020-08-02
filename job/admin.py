158d9984f289699e5eeadfe45984ba218b561975
david euler <davidullua@aliyun.com>
2020-08-02 23:15:04 +0800

    refinement on form display & list display

--- job/admin.py
+++ job/admin.py
@@ -4,7 +4,13 @@ from django import forms
 from job.models import Job
 
 class JobAdmin(admin.ModelAdmin):
-    exclude = ('creator_id','created_date','modified_date')
+    exclude = ('creator','created_date','modified_date')
+    list_display = ('job_name', 'job_type', 'job_city', 'creator', 'created_date', 'modified_date')
+
+    def save_model(self, request, obj, form, change):
+        obj.creator = request.user
+        super().save_model(request, obj, form, change)
+
 
 # Register your models here.
 admin.site.register(Job, JobAdmin)
--- job/models.py
+++ job/models.py
@@ -20,11 +20,11 @@ Cities = [
 
 
 class Job(models.Model):
-    job_type = models.CharField(max_length=250, blank=False, choices=JobTypes, verbose_name="职位类别")
+    job_type = models.SmallIntegerField(blank=False, choices=JobTypes, verbose_name="职位类别")
     job_name = models.CharField(max_length=250, blank=False, verbose_name="职位名称")
     job_city = models.SmallIntegerField(choices=Cities, blank=False, verbose_name="工作地点")
     job_responsibility = models.TextField(max_length=1024, verbose_name="职位职责")
     job_requirement = models.TextField(max_length=1024, blank=False, verbose_name="职位要求")
-    creator_id = models.CharField(max_length=250, verbose_name="创建人")
+    creator = models.ForeignKey(User, verbose_name="创建人", null=True, on_delete=models.SET_NULL)
     created_date = models.DateTimeField(verbose_name="创建日期", default=datetime.now)
     modified_date = models.DateTimeField(verbose_name="修改日期", default=datetime.now)
