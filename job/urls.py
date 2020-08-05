5be502df3609b4730bb75e9416b285b6dde9d95e
david euler <davidullua@aliyun.com>
2020-08-05 20:47:21 +0800

    commented

--- job/urls.py
+++ job/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls import url
 from job import views
 
 urlpatterns = [
+    # 职位列表
     url(r"^joblist/", views.joblist, name="joblist"),
+
+    # 职位详情
     url(r'^job/(?P<job_id>\d+)/$', views.detail, name='detail'),
+
+    # 首页自动跳转到 职位列表
+    url(r"^$", views.joblist, name="name"),
 ]
--- recruitment/urls.py
+++ recruitment/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     url(r"^", include("job.urls")),
+
     path('admin/', admin.site.urls),
 ]
