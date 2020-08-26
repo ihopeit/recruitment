0c270c1690d3df33853488cb1850502ba0c26c07
david euler <davidullua@ihopeit.com>
2020-08-26 15:58:02 +0800

    minor update for production settings

--- settings/production.py
+++ settings/production.py
@@ -1,6 +1,12 @@
 from .base import *
 
 DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+LDAP_AUTH_CONNECTION_USERNAME = "admin"
+LDAP_AUTH_CONNECTION_PASSWORD = "your_admin_credentials"
+
 INSTALLED_APPS += (
     # other apps for production site
 )
