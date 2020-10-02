68639d1225bad870d3d974fca2643e29f090d069
david euler <davidullua@aliyun.com>
2020-10-02 17:52:25 +0800

    sec40. Celery periodic tasks

--- recruitment/celery.py
+++ recruitment/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 import os
 
-from celery import Celery
+from celery import Celery, shared_task, task
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
@@ -22,3 +22,39 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+
+from celery.schedules import crontab
+
+# this is important to load the celery tasks
+from recruitment.tasks import add
+
+app.conf.beat_schedule = {
+    'add-every-10-seconds': {
+        'task': 'recruitment.tasks.add',
+        'schedule': 10.0,
+        'args': (16, 4, )
+    },
+}
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(10.0, test.s('hello'), name='hello every 10')
+
+    # Calls test('world') every 30 seconds
+    sender.add_periodic_task(30.0, test.s('world'), expires=10)
+
+    # Executes every Monday morning at 7:30 a.m.
+    sender.add_periodic_task(
+        crontab(hour=7, minute=30, day_of_week=1),
+        test.s('Happy Mondays!'),
+    )
+
+
+@app.task
+def test(arg):
+    print(arg)
+
+
+app.conf.timezone = "Asia/Shanghai"
---
+++ recruitment/tasks.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import, unicode_literals
+from celery import shared_task 
+
+@shared_task
+def add(a, b,):
+    return a + b
