b81ea83d76443614fda36f4928c736ae6535aa1a
david euler <davidullua@aliyun.com>
2020-10-04 14:17:18 +0800

    列表自动显示所有字段

--- running/admin.py
+++ running/admin.py
@@ -5,6 +5,9 @@ from .models import Country, Province, Area, City
 class ReadOnlyAdmin(admin.ModelAdmin):
     readonly_fields = []
 
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.concrete_fields]
+
     def get_readonly_fields(self, request, obj=None):
         return list(self.readonly_fields) + \
                [field.name for field in obj._meta.fields] + \
