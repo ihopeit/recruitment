ca763f87c8a22b5207d3b2703b15c30ca710ec94
david euler <davidullua@aliyun.com>
2020-10-20 00:04:01 +0800

    fix job create

--- jobs/admin.py
+++ jobs/admin.py
@@ -13,7 +13,8 @@ class JobAdmin(admin.ModelAdmin):
     list_display = ('job_name', 'job_type', 'job_city', 'creator', 'created_date', 'modified_date')
 
     def save_model(self, request, obj, form, change):
-        obj.creator = request.user
+        if obj.creator is None:
+            obj.creator = request.user
         super().save_model(request, obj, form, change)
 
 
