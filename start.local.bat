b9b738d4e75de33974e89e5be0e3c3c9854d1005
david euler <davidullua@aliyun.com>
2020-10-28 01:22:18 +0800

    sec 6.1-6.2  使用 ASGI 容器启动生产环境应用; 使用 CDN 域名

--- start.local.bat
+++ start.local.bat
@@ -3,4 +3,10 @@ django-admin compilemessages
 test -f settings/local.py || echo "===== warning: local.py does not exist, will initialize the file, please update the configs ====="
 test -f settings/local.py || cp settings/production.py settings/local.py
 test -f settings/local.py && sed -i '' 's/DEBUG = False/DEBUG = True/g' settings/local.py
+
+# synchronous web server for development:
 python manage.py runserver 0.0.0.0:8000 --settings=settings.local
+
+# for async web server:
+# export DJANGO_SETTINGS_MODULE=settings.local
+# uvicorn recruitment.asgi:application --workers 3
--- start.production.bat
+++ start.production.bat
@@ -1,2 +1,6 @@
 django-admin compilemessages
-python manage.py runserver 0.0.0.0:8000 --settings=settings.production
+# python manage.py runserver 0.0.0.0:8000 --settings=settings.production
+
+# for async web server:
+export DJANGO_SETTINGS_MODULE=settings.production
+uvicorn recruitment.asgi:application --workers 3
