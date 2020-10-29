21420a5a8408542f5b45a44e5b83e72ccf4228eb
david euler <davidullua@ihopeit.com>
2020-10-29 08:17:50 +0800

    startup script update

--- start.local.bat
+++ start.local.bat
@@ -1,11 +1,11 @@
-django-admin compilemessages
+# django-admin --settings=settings.local compilemessages 
 ## if local config file does not exist, clond one:
-test -f settings/local.py || echo "===== warning: local.py does not exist, will initialize the file, please update the configs ====="
+test -f settings/local.py || echo "=== warning: local.py does not exist, will initialize the file, please update the configs ==="
 test -f settings/local.py || cp settings/production.py settings/local.py
-test -f settings/local.py && sed -i '' 's/DEBUG = False/DEBUG = True/g' settings/local.py
+test -f settings/local.py && sed -i '' 's/DEBUG = False/DEBUG = True/g' settings/local.py 2> /dev/null
 
 # synchronous web server for development:
-python manage.py runserver 0.0.0.0:8000 --settings=settings.local
+python3 manage.py runserver 0.0.0.0:8000 --settings=settings.local
 
 # for async web server:
 # export DJANGO_SETTINGS_MODULE=settings.local
--- start.production.bat
+++ start.production.bat
@@ -1,6 +1,7 @@
-django-admin compilemessages
+# django-admin compilemessages
 # python manage.py runserver 0.0.0.0:8000 --settings=settings.production
 
 # for async web server:
 export DJANGO_SETTINGS_MODULE=settings.production
-uvicorn recruitment.asgi:application --workers 3
+uvicorn recruitment.asgi:application --port 8001 --workers 2
+#uvicorn recruitment.asgi:application --port 8001 --workers 2
