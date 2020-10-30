54c06d493e900305de27a5fed9a872e462ae850c
david euler <davidullua@aliyun.com>
2020-10-30 23:58:00 +0800

    database metrics for prometheus

--- settings/base.py
+++ settings/base.py
@@ -194,7 +194,8 @@ WSGI_APPLICATION = 'recruitment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django_prometheus.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
     'running': {
