877e674ffa1e1ae4ac66d4e963ae3525a93cbc2c
david euler <davidullua@aliyun.com>
2020-12-02 02:28:16 +0800

    for kubernetes yamls

--- build.images.sh
+++ build.images.sh
@@ -3,3 +3,10 @@
 
  ## or just docker pull already built image from docker hub:
  # docker pull ihopeit/django-recruitment:0.8
+
+## 构建包含 local.py 文件的 0.9 版本镜像：
+cp settings/local.py settings/local-setting.py
+docker build -f Dockerfile-local-setting -t ihopeit/django-recruitment:0.9 .
+rm settings/local-setting.py
+
+sudo docker tag ihopeit/django-recruitment:0.9 registry.cn-beijing.aliyuncs.com/ihopeit/django-recruitment:0.9
