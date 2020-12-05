b2c6638063c478ed11d54f1b1b99c0b70040c99a
david euler <davidullua@aliyun.com>
2020-12-06 01:24:52 +0800

    alway pull & use production config for flower

--- flower.start.sh
+++ flower.start.sh
@@ -1,1 +1,1 @@
-DJANGO_SETTINGS_MODULE=settings.local celery -A recruitment flower
+DJANGO_SETTINGS_MODULE=settings.production celery -A recruitment flower
--- worker.start.sh
+++ worker.start.sh
@@ -1,2 +1,4 @@
-DJANGO_SETTINGS_MODULE=settings.local celery -A recruitment worker -l INFO
+# 启动 recruitment 这个 package 的时候，会运行 __init__.py
+# __init__.py 里面初始化了 django 的配置
+DJANGO_SETTINGS_MODULE=settings.production celery -A recruitment worker -l INFO
 
